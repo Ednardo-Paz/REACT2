255c85e8eff241f9183e18e51e4fb19eeab90d6a
Ednardo Paz <eds_paz@hotmail.com>
2021-06-22 20:32:50 -0300

    Slide react
    Slide react

--- appreact/appreact5/src/App.js
+++ appreact/appreact5/src/App.js
@@ -1,12 +1,30 @@
 import React from 'react'
+import Slide from './Slide'
+
+
+
 
 const App = () => {
-const [texte]
 
+  const slides = [ 
+    {
+      id: "slide 1",
+      text: "slide 1"
+    },
+    {
+      id: "slide 2",
+      text: "slide 2"
+    },
+    {
+      id: "slide 3",
+      text: "slide 3"
+    },
+  ]
+   
 
   return (
     <div>
-      
+      <Slide  slides={slides}/>
     </div>
   )
 }
---
+++ appreact/appreact6/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+const [texte]
+
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App
